42920ea451ac8421c0a36b88b281a2fd3f754212
SiddharajYadav <sidharajyadav157@gmail.com>
2024-12-14 10:44:42 +0530

    Update authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const authenticate = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Unauthorized" });
+  if (!token) return res.status(401).json({ error: "Unauthorized" }); 
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
