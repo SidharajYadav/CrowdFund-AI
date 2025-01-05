b6a259b31e995c78690efc1153571ede14cb7c31
SiddharajYadav <sidharajyadav157@gmail.com>
2025-01-05 14:14:42 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,5 +31,5 @@ app.use((err, req, res, next) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`) 
 );
