7dcd3a4959caf19334036096b04f1dcccaef4a75
SiddharajYadav <sidharajyadav157@gmail.com>
2025-01-08 21:26:55 +0530

    Update Campaign.js

--- backend/src/models/Campaign.js
+++ backend/src/models/Campaign.js
@@ -15,4 +15,4 @@ const campaignSchema = new mongoose.Schema(
   { timestamps: true }
 );
  
-export default mongoose.model("Campaign", campaignSchema);
+export default mongoose.model("Campaign", campaignSchema); 
