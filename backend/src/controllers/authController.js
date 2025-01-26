24c3b5a2a435091161bdddc860e6873cba5d212a
SiddharajYadav <sidharajyadav157@gmail.com>
2025-01-26 11:32:37 +0530

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -13,7 +13,7 @@ export const register = async (req, res) => {
     res.status(201).json({ message: "User registered successfully!" });
   } catch (err) {
     res.status(500).json({ error: "Registration failed" });
-  }
+  } 
 };
 
 export const login = async (req, res) => {
