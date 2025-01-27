b204694237b6d880adb011051d4f47b906ded408
SiddharajYadav <sidharajyadav157@gmail.com>
2025-01-27 14:48:29 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+ 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/campaigns", campaignRoutes);
