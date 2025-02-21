149b45c79b772e66b2ec3b7b9d8a315f10f2af57
SiddharajYadav <sidharajyadav157@gmail.com>
2025-02-21 09:33:20 +0530

    Update model_training.py

--- ml/src/model_training.py
+++ ml/src/model_training.py
@@ -10,7 +10,7 @@
 
 #     # Split data
 #     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+ 
 #     # Train model
 #     model = RandomForestClassifier(n_estimators=100, random_state=42)
 #     model.fit(X_train, y_train)
