6b287cd886bd7ce5ec6c1af47ba37dadaaae2826
SiddharajYadav <sidharajyadav157@gmail.com>
2025-03-20 09:12:02 +0530

    Update model_training.py

--- ml/src/model_training.py
+++ ml/src/model_training.py
@@ -8,7 +8,7 @@
 #     X = df[['goal', 'duration', 'category', 'description_length']]
 #     y = df['state']
 
-#     # Split data
+#     # Split data 
 #     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
  
 #     # Train model
