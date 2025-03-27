09d909e1a960c2631bc0b47d6133a23748519724
SiddharajYadav <sidharajyadav157@gmail.com>
2025-03-27 17:43:03 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express(); 
  
 // Middleware
-app.use(cors());
+app.use(cors()); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
